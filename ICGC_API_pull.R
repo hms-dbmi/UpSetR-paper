857521fa1ee89660e632d52f24c74f762fafcfe5
Jake Conway <jakeconway45@gmail.com>
2017-03-25 14:21:33 -0400

    Update ICGC_API_pull.R

--- ICGC_API_pull.R
+++ ICGC_API_pull.R
@@ -4,20 +4,20 @@ require(plyr)
 
 #An example of the function input. 
 #Specify the project name, fields, and number of entries(size) to pull in each list
-#This version of pulling from the ICGC REST API specifcally checks whether the mutations affected a coding region in the transcripts field\
-#If the mutation affects a non-coding region, it is filtered out of the data frame
-data <- list(list(project = "THCA-US", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 6659),
-             list(project = "THCA-SA", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 45126),
-             list(project = "LUSC-US", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 65063),
-             list(project = "LUSC-KR", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 64671),
-             list(project = "LUSC-CN", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 419),
-             list(project = "LAML-KR", fields = c("id", "mutation", "chromosome", "start", "end", "transcripts"), size = 42977))
+
+data <- list(list(project = "THCA-US", fields = c("id", "mutation", "chromosome", "start", "end"), size = 6659),
+             list(project = "ORCA-IN", fields = c("id", "mutation", "chromosome", "start", "end"), size = 13626),
+             list(project = "THCA-SA", fields = c("id", "mutation", "chromosome", "start", "end"), size = 45126),
+             list(project = "LUSC-US", fields = c("id", "mutation", "chromosome", "start", "end"), size = 65063),
+             list(project = "LUSC-KR", fields = c("id", "mutation", "chromosome", "start", "end"), size = 64671),
+             list(project = "LUSC-CN", fields = c("id", "mutation", "chromosome", "start", "end"), size = 419),
+             list(project = "KIRC-US", fields = c("id", "mutation", "chromosome", "start", "end"), size = 26371),
+             list(project = "LAML-KR", fields = c("id", "mutation", "chromosome", "start", "end"), size = 42977))
 
 #Function to pull data from ICGC Rest API and convert to UpSetR data frame
 
 icgcData <- function(data){
   aggregateData <- data.frame()
-  geneIdCol <- data.frame(ensembl_id = character())
   projectNameCol <- data.frame()
   IDs <- list()
   
@@ -55,42 +55,6 @@ icgcData <- function(data){
                     "&size=",as.character(count),"&&order=desc")
       
       temp <- fromJSON(url)$hits
-      geneID_vector <- c()
-      length(geneID_vector) <- length(temp$transcripts)
-      for(k in 1:length(temp$transcripts)){
-        if("protein_coding" %in% temp$transcripts[[k]]$type){
-          if (!is.null(temp$transcripts[[k]]$consequence$geneAffectedId)) {
-          geneID <- count(temp$transcripts[[k]]$consequence$geneAffectedId)
-          names(geneID) <- c("ensembl_id", "freq")
-          geneID <- geneID[order(geneID$freq, decreasing = T), ]
-          geneID$ensembl_id <- as.character(geneID$ensembl_id)
-          geneID <- geneID[1, 1]
-          geneID_vector[k] <- geneID
-          }
-          else{
-            geneID_vector[k] <- NA
-          }
-          temp$transcripts[[k]] <- "protein_coding"
-        }
-        else {
-          if (!is.null(temp$transcripts[[k]]$consequence$geneAffectedId)) {
-            geneID <- count(temp$transcripts[[k]]$consequence$geneAffectedId)
-            names(geneID) <- c("ensembl_id", "freq")
-            geneID <- geneID[order(geneID$freq, decreasing = T),]
-            geneID$ensembl_id <- as.character(geneID$ensembl_id)
-            geneID <- geneID[1, 1]
-            geneID_vector[k] <- geneID
-          }
-          else{
-            geneID_vector[k] <- NA
-          }
-          temp$transcripts[[k]] <- "non_coding"
-        }
-      }
-      
-      geneID_vector <- as.data.frame(matrix(geneID_vector, ncol = 1))
-      colnames(geneID_vector) <- "ensembl_id"
-      geneIdCol <- rbind(geneIdCol, geneID_vector)
       aggregateData <- rbind(aggregateData, temp)
       IDs[[i]] <- c(IDs[[i]], as.vector(temp$id))
       
@@ -103,7 +67,6 @@ icgcData <- function(data){
     projectNameCol <- rbind(projectNameCol, addcolumn)
   }
   
-  aggregateData <- cbind(aggregateData, geneIdCol)
   aggregateData <- cbind(aggregateData, projectNameCol)
   
   num <- nrow(aggregateData)
@@ -119,7 +82,6 @@ icgcData <- function(data){
     projects[i] <- name
     
   }
-
   aggregateData <- aggregateData[-which(duplicated(aggregateData$id)), ]
   
   for(i in 1:length(data)){
@@ -135,13 +97,8 @@ icgcData <- function(data){
 
 
 #Run the function to generate a data frame compatible with UpSetR
+
 myData <- icgcData(data)
-
-#Change transcripts column from list to vector
-myData$transcripts <- unlist(myData$transcripts)
-
-#Only grab mutations that affect protein coding regions
-myData <- myData[which(myData$transcripts == "protein_coding"), ]
 
 setdata <- data.frame(
   projects= c("THCA-US", "THCA-SA", "ORCA-IN", "BLCA-US",
@@ -149,12 +106,12 @@ setdata <- data.frame(
               "KIRC-US", "KIRP-US", "LAML-KR"),
   Donors = c(507, 15, 119, 412, 103, 10, 504, 36, 537, 291, 171),
   Site = c("Head & Neck", "Head & Neck", "Head & Neck",
-           "Bladder", "Bladder", "Lung", "Lung", "Lung", "Kidney",
-           "Kidney", "Blood"),
+               "Bladder", "Bladder", "Lung", "Lung", "Lung", "Kidney",
+               "Kidney", "Blood"),
   Country = c("United States", "Saudi Arabia", "India", "United States",
               "China", "China", "United States", "South Korea",
               "United States", "United States", "South Korea")
-)
+  )
 
 setdata$Country <- as.character(setdata$Country)
 setdata$Site <- as.character(setdata$Site)
